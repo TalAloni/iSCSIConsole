8998b4f3062f25fde2de8b0611b730c1152b89b3
Tal Aloni <tal.aloni.il@gmail.com>
2017-03-07 10:38:49 +0200

    Updated SocketUtils

--- ISCSI/Utilities/SocketUtils.cs
+++ ISCSI/Utilities/SocketUtils.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012-2015 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2012-2017 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -14,6 +14,25 @@ namespace Utilities
 {
     public class SocketUtils
     {
+        public static void SetKeepAlive(Socket socket, TimeSpan timeout)
+        {
+            // The default settings when a TCP socket is initialized sets the keep-alive timeout to 2 hours and the keep-alive interval to 1 second.
+            SetKeepAlive(socket, true, timeout, TimeSpan.FromSeconds(1));
+        }
+
+        /// <param name="timeout">the timeout, in milliseconds, with no activity until the first keep-alive packet is sent</param>
+        /// <param name="interval">the interval, in milliseconds, between when successive keep-alive packets are sent if no acknowledgement is received</param>
+        public static void SetKeepAlive(Socket socket, bool enable, TimeSpan timeout, TimeSpan interval)
+        {
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+            // https://msdn.microsoft.com/en-us/library/dd877220.aspx
+            byte[] tcp_keepalive = new byte[12];
+            LittleEndianWriter.WriteUInt32(tcp_keepalive, 0, Convert.ToUInt32(enable));
+            LittleEndianWriter.WriteUInt32(tcp_keepalive, 4, (uint)timeout.TotalMilliseconds);
+            LittleEndianWriter.WriteUInt32(tcp_keepalive, 8, (uint)interval.TotalMilliseconds);
+            socket.IOControl(IOControlCode.KeepAliveValues, tcp_keepalive, null);
+        }
+
         /// <summary>
         /// Socket will be forcefully closed, all pending data will be ignored, and socket will be deallocated.
         /// </summary>
