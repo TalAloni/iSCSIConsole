b2b4f259c1b403d2f0b2bdf647cc90acc7c0874d
TalAloni <tal.aloni.il@gmail.com>
2021-02-26 18:39:06 +0200

    Added VS2019 ILMerge script

---
+++ ISCSIConsole/ILMerge/ILMerge.bat
@@ -0,0 +1,11 @@
+IF ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles%\Microsoft\ILMerge"
+IF NOT ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles(x86)%\Microsoft\ILMerge"
+
+set TargetFramework=%1
+if %TargetFramework%==net20 set TargetPlaform=2.0
+if %TargetFramework%==net40 set TargetPlaform=4.0
+if %TargetFramework%==net472 set TargetPlaform=4.0
+set binaryPath=%CD%\..\bin\Release\%TargetFramework%
+set outputPath=%CD%\..\bin\ILMerge\%TargetFramework%
+IF NOT EXIST "%outputPath%" MKDIR "%outputPath%"
+%ilmergePath%\ilmerge /targetplatform=%TargetPlaform% /ndebug /target:winexe /out:"%outputPath%\ISCSIConsole.exe" "%binaryPath%\ISCSIConsole.exe" "%binaryPath%\DiskAccessLibrary.FileSystems.Abstractions.dll" "%binaryPath%\DiskAccessLibrary.dll" "%binaryPath%\DiskAccessLibrary.Win32.dll" "%binaryPath%\ISCSI.dll"
